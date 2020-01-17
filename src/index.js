0ced6448bbc6ca9de15bcff9bea735b430f80213
Denner Lemos <dennerlemos91@gmail.com>
2020-01-16 23:11:02 -0300

    Inclusão do cors

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const config = require('./config/enviroments');
 
 const routes = require('./routes');
 
 mongoose.connect(
-  'mongodb+srv://radardev:radardev@cluster0-4a2nr.mongodb.net/radardev?retryWrites=true&w=majority',
+  'mongodb+srv://radardev:radardev@radardev-4a2nr.mongodb.net/radardev?retryWrites=true&w=majority',
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
@@ -21,6 +22,7 @@ mongoose.connect(
 
 const app = express();
 app.use(express.json());
+app.use(cors());
 app.use(config.contextPath, routes);
 
 app.listen(config.port, () => {
